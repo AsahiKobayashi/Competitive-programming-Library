2ad40ff248a36e0271edeac7763c5757a8c52e8d
AsahiKobayashi <109237514+AsahiKobayashi@users.noreply.github.com>
2023-11-21 10:53:46 +0900

    Update エラトステネス.java

--- Math/エラトステネス.java
+++ Math/エラトステネス.java
@@ -1,14 +1,14 @@
 boolean [] Eratosthenes(){
         int limit = 10101010;
-        boolean [] isprime = new boolean[limit+1];
+        boolean [] isprime = new boolean[limit + 1];
         Arrays.fill(isprime,true);
         isprime[0] = false;
         isprime[1] = false;
-        for(int i = 2 ; i <= limit ; i++ ){
+        for(int i = 2 ; i <= limit ; i ++) {
             if(!isprime[i]) continue;
-            for(int j = i * 2 ; j <= limit ; j += i ){
+            for(int j = i * 2 ; j <= limit ; j += i){
                 isprime[j] = false;
             }
         }
         return isprime;
-    }
+}
