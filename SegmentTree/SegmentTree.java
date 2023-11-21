017b4b0df9c0915d0512a540a8d1d11317847ab7
AsahiKobayashi <109237514+AsahiKobayashi@users.noreply.github.com>
2023-11-22 02:44:25 +0900

    Update SegmentTree.java

--- SegmentTree/SegmentTree.java
+++ SegmentTree/SegmentTree.java
@@ -1,46 +1,41 @@
-class SegmentTree{
-        
-        private int n ;
-        private long init ;
-        private long [] tree ;
-        private int size ;
-        private BinaryOperator<Long> op ;
+class SegmentTree <T> {
 
-        SegmentTree(int size , long init , BinaryOperator<Long> op){
-            this.size = size;
-            this.init = init ;
-            this.op = op ;
-            n = 1 ;
-            while(n <= size) n *= 2 ;
-            this.tree = new long[2 * n];
-            for(int i = 0 ; i < 2 * n ; i ++ ) tree[i] = init ;
+    private int n ;
+    private T init ;
+    private T [] tree ;
+    private int size ;
+    private BinaryOperator<T> op ;
+
+    @SuppressWarnings ("unchecked")
+    SegmentTree(int size , T init , BinaryOperator<T> op) {
+        this.size = size;
+        this.init = init ;
+        this.op = op ;
+        n = 1 ;
+        while(n <= size) n *= 2 ;
+        this.tree = (T []) new Object[2 * n];
+        for(int i = 0 ; i < 2 * n ; i ++ ) tree[i] = init ;
+    }
+
+    public void update(int index , T value) {
+        index += n ;
+        tree[index] = value ;
+        while(index > 0) {
+            index /= 2 ;
+            tree[index] = op.apply(tree[2 * index] , tree[2 * index + 1]);
         }
+    }
+
+    public T query(int l , int r){
+        l += n ; r += n ;
+        T res = init ;
+        while(l < r){
+            if(l % 2 == 1){ res = op.apply(res , tree[l]) ; l ++ ; }
+            l /= 2 ;
+            if(r % 2 == 1){ res = op.apply(res , tree[r - 1]) ; r -- ; }
+            r /= 2 ;
+        }
+        return res ;
+    }
         
-        public void update(int index , long value){
-            index += n ;
-            tree[index] = value ;
-            while(index > 0){
-                index /= 2 ;
-                tree[index] = op.apply(tree[2 * index] , tree[2 * index + 1]);
-            }
-        }
-        
-        public long query(int l , int r){
-            l += n ; r += n ;
-            long res = init ;
-            while(l < r){
-                if(l % 2 == 1){ res = op.apply(res , tree[l]) ; l ++ ; }
-                l /= 2 ;
-                if(r % 2 == 1){ res = op.apply(res , tree[r - 1]) ; r -- ; }
-                r /= 2 ;
-            }
-            return res ;
-        }
-        
-        public void debug() {
-            for(int i = n ; i < n + size ; i ++ ) {
-                System.out.print(tree[i]+" ");
-            }
-            System.out.println();
-        }
 }
