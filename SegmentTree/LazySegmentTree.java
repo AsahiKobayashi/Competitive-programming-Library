0610104a1ad5bd1bf05eac02ac69bbef7c6f68ca
AsahiKobayashi <109237514+AsahiKobayashi@users.noreply.github.com>
2023-11-21 10:41:18 +0900

    Update LazySegmentTree.java

--- SegmentTree/LazySegmentTree.java
+++ SegmentTree/LazySegmentTree.java
@@ -1,4 +1,5 @@
 class LazySegmentTree {
+    
     final long[] Dat;
     final long[] Laz;
     final int N;
@@ -9,7 +10,7 @@ class LazySegmentTree {
     final LongBinaryOperator H;
     final LongBinaryOperator P;
     final int[] Stack = new int[64];
-    //初期配列 , dat(関与しない値) , lazy(関与しない値) , dat() , dat&lazy() , lazy() , lazy*len()
+
     public LazySegmentTree(int n, long e0, long e1, LongBinaryOperator f, LongBinaryOperator g, LongBinaryOperator h, LongBinaryOperator p) {
         this.E0 = e0;
         this.E1 = e1;
@@ -22,14 +23,17 @@ class LazySegmentTree {
         Arrays.fill(Dat, E0);
         Arrays.fill(Laz, E1);
     }
+    
     public LazySegmentTree(long[] src, long e0, long e1, LongBinaryOperator f, LongBinaryOperator g, LongBinaryOperator h, LongBinaryOperator p) {
         this(src.length, e0, e1, f, g, h, p);
         build(src);
     }
+    
     private void build(long[] src) {
         System.arraycopy(src, 0, Dat, N, src.length);
         for (int i = N - 1; i > 0; i--) Dat[i] = F.applyAsLong(Dat[i << 1 | 0], Dat[i << 1 | 1]);
     }
+    
     public void update(int l, int r, long v) {
         if (l >= r) return;
         int m = updown(l, r);
@@ -43,6 +47,7 @@ class LazySegmentTree {
             Dat[k] = F.applyAsLong(calcDat(k << 1 | 0), calcDat(k << 1 | 1));
         }
     }
+    
     public long query(int i) {
         int k = 1;
         int l = 0, r = N;
@@ -56,6 +61,7 @@ class LazySegmentTree {
         }
         return Dat[k];
     }
+    
     public long query(int l, int r) {
         if (l >= r) return E0;
         updown(l, r);
@@ -66,6 +72,7 @@ class LazySegmentTree {
         }
         return F.applyAsLong(resL, resR);
     }
+    
     private int updown(int l, int r) {
         if (l >= r) return 0;
         int i = 0;
@@ -79,6 +86,7 @@ class LazySegmentTree {
         while (i > 0) calcDat(Stack[--i]);
         return m;
     }
+    
     private long calcDat(int k) {
         long lz = Laz[k];
         if (lz != E1) {
@@ -93,12 +101,12 @@ class LazySegmentTree {
         }
         return Dat[k];
     }
-    void debug()
-    {
-        for(int i = Dat.length - N ; i < Dat.length - 1; i ++ ) 
-        {
+    
+    public void debug() {
+        for(int i = Dat.length - N ; i < Dat.length - 1; i ++ ) {
             System.out.print(Dat[i]+" ");
         }
         System.out.println();
     }
+    
 }
