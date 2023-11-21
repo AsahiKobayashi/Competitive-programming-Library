e13ef72224d69bd905445a58e5c1b17644ec2863
AsahiKobayashi <109237514+AsahiKobayashi@users.noreply.github.com>
2023-11-22 02:59:20 +0900

    SegmentTree2D

--- SegmentTree/SegmentTree2D.java
+++ SegmentTree/SegmentTree2D.java
@@ -1,16 +1,16 @@
 /* https://nyaannyaan.github.io/library/data-structure-2d/2d-segment-tree.hpp
- * Nyaanさんのcppコードをjavaにポーティングさせていただきました.
- */
-class SegmentTree2D {
+* Nyaanさんのcppコードをjavaにポーティングさせていただきました.
+*/
+class SegmentTree2D <T> {
 
     private int H, W;
-    private long [] seg;
-    private LongBinaryOperator f;
-    private long I;
+    private T [] seg;
+    private BinaryOperator<T> op ;
+    private T e ;
 
-    SegmentTree2D(int h, int w, LongBinaryOperator f, long I) {
-        this.f = f;
-        this.I = I;
+    SegmentTree2D(int h, int w, T e , BinaryOperator<T> op) {
+        this.op = op;
+        this.e = e;
         init(h, w);
     }
 
@@ -18,15 +18,15 @@ class SegmentTree2D {
         H = W = 1;
         while (H < h) H <<= 1;
         while (W < w) W <<= 1;
-        seg = new long[4 * H * W];
-        Arrays.fill(seg, I);
+        seg = (T []) new Object[4 * H * W];
+        Arrays.fill(seg, e);
     }
 
     private int id(int h, int w) {
         return h * 2 * W + w;
     }
 
-    public void set(int h, int w, long x) {
+    public void set(int h, int w, T x) {
         seg[id(h + H, w + W)] = x;
     }
     
@@ -34,67 +34,68 @@ class SegmentTree2D {
     public void build() {
         for (int w = W; w < 2 * W; w++) {
             for (int h = H - 1; h > 0; h--) {
-                seg[id(h, w)] = f.applyAsLong(seg[id(2 * h, w)], seg[id(2 * h + 1, w)]);
+                seg[id(h, w)] = op.apply(seg[id(2 * h, w)], seg[id(2 * h + 1, w)]);
             }
         }
         for (int h = 0; h < 2 * H; h++) {
             for (int w = W - 1; w > 0; w--) {
-                seg[id(h, w)] = f.applyAsLong(seg[id(h, 2 * w)], seg[id(h, 2 * w + 1)]);
+                seg[id(h, w)] = op.apply(seg[id(h, 2 * w)], seg[id(h, 2 * w + 1)]);
             }
         }
     }
     @SuppressWarnings("unchecked")
-    public long get(int h, int w) {
+    public T get(int h, int w) {
         return seg[id(h + H, w + W)];
     }
     @SuppressWarnings("unchecked")
-    public void update(int h, int w, long x) {
+    public void update(int h, int w, T x) {
         h += H;
         w += W;
         seg[id(h, w)] = x;
         for (int i = h >> 1; i > 0; i >>= 1) {
-            seg[id(i, w)] = f.applyAsLong(seg[id(2 * i, w)] , seg[id(2 * i + 1, w)]);
+            seg[id(i, w)] = op.apply(seg[id(2 * i, w)] , seg[id(2 * i + 1, w)]);
         }
         for (; h > 0; h >>= 1) {
             for (int j = w >> 1; j > 0; j >>= 1) {
-                seg[id(h, j)] = f.applyAsLong(seg[id(h, 2 * j)], seg[id(h, 2 * j + 1)]);
+                seg[id(h, j)] = op.apply(seg[id(h, 2 * j)], seg[id(h, 2 * j + 1)]);
             }
         }
     }
 
     @SuppressWarnings("unchecked")
-    private long _innerQuery(int h, int w1, int w2) {
-        long res = I;
+    private T _innerQuery(int h, int w1, int w2) {
+        T res = e ;
         for (; w1 < w2; w1 >>= 1, w2 >>= 1) {
             if ((w1 & 1) != 0) {
-                res = f.applyAsLong(res, seg[id(h, w1)]);
+                res = op.apply(res, seg[id(h, w1)]);
                 w1++;
             }
             if ((w2 & 1) != 0) {
                 w2--;
-                res = f.applyAsLong(res, seg[id(h, w2)]);
+                res = op.apply(res, seg[id(h, w2)]);
             }
         }
         return res;
     }
 
-    public long query(int h1, int w1, int h2, int w2) {
-        if (h1 >= h2 || w1 >= w2) return I;
-        long res = I;
+    public T query(int h1, int w1, int h2, int w2) {
+        if (h1 >= h2 || w1 >= w2) return e ;
+        T res = e ;
         h1 += H;
         h2 += H;
         w1 += W;
         w2 += W;
         for (; h1 < h2; h1 >>= 1, h2 >>= 1) {
             if ((h1 & 1) != 0) {
-                res = f.applyAsLong(res, _innerQuery(h1, w1, w2));
+                res = op.apply(res, _innerQuery(h1, w1, w2));
                 h1++;
             }
             if ((h2 & 1) != 0) {
                 h2--;
-                res = f.applyAsLong(res, _innerQuery(h2, w1, w2));
+                res = op.apply(res, _innerQuery(h2, w1, w2));
             }
         }
         return res;
     }
+    
 }
