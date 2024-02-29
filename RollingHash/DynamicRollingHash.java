223132f64e1bfaff282c4332066d3f7f29fefe10
AsahiKobayashi <109237514+AsahiKobayashi@users.noreply.github.com>
2024-02-29 18:27:12 +0900

    Update DynamicRollingHash.java

--- RollingHash/DynamicRollingHash.java
+++ RollingHash/DynamicRollingHash.java
@@ -1,61 +1,99 @@
-    record HASH(long a , long b) { }
+class DynamicRollingHash {
 
-    class RollingHash {
-    
-        private static final int base1 = 1007 , base2 = 2009 ;
-        private static final int mod1 = 1000000007 , mod2 = 1000000009;
-        private long [] hash1 , hash2 , power1 , power2 ;
-        private HASH hash ;
-        private int n , length ;
+    private final int n ;
+    private final int size ;
+    private final BinaryOperator<HashData> op ;
+    private final HashData e ;
+    private final HashData [] data;
+    final long base = 31 ;
+    final long Hash_MOD = 1000000009;
+    private long [] power ;
+    private HashData [] array ;
 
-        RollingHash(HASH hash , int length) {
-            this.hash = hash ;
-            this.length = length;
-        }
+    @SuppressWarnings("unchecked")
 
-        RollingHash(final String s) {
-            this.n = s.length() ;
-            hash1 = new long[n + 1];
-            hash2 = new long[n + 1];
-            power1 = new long[n + 1];
-            power2 = new long[n + 1];
-            Arrays.fill(power1 , 1L);
-            Arrays.fill(power2 , 1L);
-            for(int i = 0 ; i < n ; i ++) {
-                hash1[i + 1] = (hash1[i] * base1 + s.charAt(i)) % mod1 ;
-                hash2[i + 1] = (hash2[i] * base2 + s.charAt(i)) % mod2 ;
-                power1[i + 1] = (power1[i] * base1) % mod1 ;
-                power2[i + 1] = (power2[i] * base2) % mod2 ;
-            }
-            this.hash = get(0 , s.length()).hash ;
-            this.length = s.length() ;
+    public DynamicRollingHash(String s) {
+        this.n = s.length() ;
+        power = new long[n + 1];
+        array = new HashData[n];
+        power[0] = 1 ;
+        for(int i = 1 ; i < power.length ; i ++) {
+            power[i] = power[i - 1] * base % Hash_MOD ; 
         }
-        // 区間のハッシュを取得
-        public RollingHash get(int l , int r) {
-            long res1 = hash1[r] - hash1[l] * power1[r - l] % mod1 ;
-            if(res1 < 0) res1 += mod1 ;
-            long res2 = hash2[r] - hash2[l] * power2[r - l] % mod2 ;
-            if(res2 < 0) res2 += mod2;
-            return new RollingHash(new HASH(res1 , res2) , r - l);
+        for(int i = 0 ; i < n ; i ++) {
+            HashData o = new HashData();
+            o.hash = s.charAt(i) ;
+            o.length = 1 ;
+            array[i] = o ;
         }
-        // 単一文字列内のマージ
-        public RollingHash merge(int l1 , int r1 , int l2 , int r2) {
-            HASH left = get(l1 , r1).hash , right = get(l2 , r2).hash ;
-            long res1 = (right.a + left.a * power1[r2 - l2]) % mod1 ;
-            long res2 = (right.b + left.b * power2[r2 - l2]) % mod2 ;
-            return new RollingHash(new HASH(res1 , res2) , (r2 - l2) + (r1 - l1));
-        }
-        //　複数の文字列間のマージ
-        public RollingHash merge(RollingHash l , RollingHash r) {
-            HASH left = l.hash , right = r.hash ;
-            long res1 = (right.a + left.a * power1[r.length]) % mod1 ;
-            long res2 = (right.b + left.b * power2[r.length]) % mod2 ;
-            return new RollingHash(new HASH(res1, res2), l.length + r.length);
-        }
-
-        @Override
-        public String toString() {
-            return "hash : " + this.hash.a+" - "+this.hash.b +" len : "+ this.length ;
-        }
-        
+        int k = 1;
+        while (k < n) k <<= 1;
+        this.size = k;
+        this.e = new HashData() ;
+        this.op = (a , b) -> {
+            HashData o = new HashData();
+            o.hash = (a.hash + b.hash * power[a.length]) % Hash_MOD ;
+            o.length = a.length + b.length ;
+            return o ;
+        };
+        this.data = new HashData[size << 1];
+        Arrays.fill(data, e);
+        build(array);
     }
+
+    private void build(HashData [] dat) {
+        int l = dat.length;
+        System.arraycopy(dat, 0, data, size, l);
+        for (int i = size - 1; i > 0; i--) {
+            data[i] = op.apply(data[i << 1 | 0], data[i << 1 | 1]);
+        }
+    }
+
+    private void update(int p , HashData x) {
+        assert 0 <= p && p < n : "p=" + p;
+        data[p += size] = x;
+        p >>= 1;
+        while (p > 0) {
+            data[p] = op.apply(data[p << 1 | 0], data[p << 1 | 1]);
+            p >>= 1;
+        }
+    }
+
+    public void update(int p , char c) {
+        HashData o = query(p);
+        o.hash = c ;
+        update(p , o);
+    } 
+
+    public long hash(int l , int r) {
+        return query(l, r).hash ;
+    }
+
+    private HashData query(int p) {
+        assert 0 <= p && p < n : "p=" + p;
+
+        return data[p + size];
+    }
+
+    private HashData query(int l, int r) {
+        assert 0 <= l && l <= r && r <= n : "l=" + l + ", r=" + r;
+
+        HashData sumLeft = e;
+        HashData sumRight = e;
+        l += size;
+        r += size;
+        while (l < r) {
+            if ((l & 1) == 1) sumLeft = op.apply(sumLeft, data[l++]);
+            if ((r & 1) == 1) sumRight = op.apply(data[--r], sumRight);
+            l >>= 1;
+            r >>= 1;
+        }
+        return op.apply(sumLeft, sumRight) ;
+    }
+
+    private class HashData {
+        long hash ;
+        int length ;
+    }
+
+}
